2d96646f715b87fecf4c4e5e3e6a5ab543c2c1c4
ReacherZhouCoder <115893260+ReacherZhouCoder@users.noreply.github.com>
2022-10-16 03:52:13 +0800

    Add files via upload

---
+++ c study/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  c study
+//
+//  Created by richerd on 2022/10/16.
+//
+
+
+#include<stdio.h>
+int main()
+{
+    printf("     **\n");
+    printf("     **\n");
+    printf("************\n");
+    printf("************\n");
+    printf("    *  *   \n");
+    printf("    *  *   \n");
+return 0;
+}
